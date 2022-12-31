678db31e68a2cd78205931aa39479250bd0ee835
Marwan <kaka.mer1998@gmail.com>
2022-12-31 23:44:57 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function countDown(n) {
+  //   for (n; n < 10; n--) {
+  //   console.log(n);
+  // }
+  while (n >= 0) {
+    console.log(n--);
+  }
+}
+function writeCards(arr){
+  for(let i = 0;i in arr; i++){
+    return `"Thank you, "  ${arr[i]}, for the wonderful surprise gift!`;
+  } 
+}
